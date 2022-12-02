f53ad8f86a42897280290d03c56a43eed97eac36
Lorenzo Salvi <lorenzosalvi@iMac-di-Lorenzo.homenet.telecomitalia.it>
2022-12-02 11:46:36 +0100

    esempio script 05 e modifica script 03

--- 03-espressioni-e-istruzioni.py
+++ 03-espressioni-e-istruzioni.py
@@ -34,3 +34,38 @@ parola_tre = 'stringa'
 
 print(parola_uno + ' ' + parola_due + ' '+ parola_tre) # stampa: Sono una stringa
 print(parola_uno *3) # stampa SonoSonoSono
+
+# operatori di divisione intera e modulo
+# // è l'operatore di divisione intera: divide due numeri e arrotonda il risultato all'interno inferiore
+# % è l'operazione di modulo: che restituisce il resto dell'operazione di divisione tra due numeri interi
+
+# esempio
+
+minuti = 105
+ore1 = minuti / 60
+ore2 = minuti // 60
+
+print(minuti, ' minuti in ore, sono: ', ore1) # stampa: '105 minuti in ore, sono: 1,75'
+print(minuti, ' minuti in ore, sono: ', ore2, '(con operazione di div. intera)') # stampa: '105 minuti in ore, sono: 1 (con operazione di div. intera)'
+
+# esempio
+
+resto = minuti % 60
+
+print(resto)    # stampa 45
+
+
+# espressioni booleane: sono espressioni che possono valere 'vero' o 'falso'
+# gli operatori sono: '==', '!=', '>', '>=', '<', '<='
+
+print(5 == 5)   # True
+print(5 == 7)   # False
+print(16 > 5)   # True
+
+# operatori logici
+# sono 'and', 'or', 'not'
+
+
+
+
+
---
+++ 05-istruzioni-condizionali-e-ricorsione.py
@@ -0,0 +1,61 @@
+
+# istruzione condizionale 'if'
+#
+# forma:
+# if (condizione_booleana):
+#       condizione vera
+# else:
+#       condizione falsa
+
+
+# esecuzione semplificata
+#
+# forma:
+# if (condizione_booleana):
+#       condizione vera
+
+
+# condizione in serie
+#
+# forma:
+# if (condizione1):
+#       cond1
+# elif (condizione2):
+#       cond2
+# elif (condizione3):
+#       cond3
+
+
+# esempio
+
+x = 7
+
+if (x > 10):
+    print('Vero ', x, ' è maggiore di 10')
+else:
+    print('Falso ', x, ' è minore di 10')
+
+x = 15
+
+if (x > 15):
+    print('Sono nel caso 1')
+elif (x == 15):
+    print('Sono nel caso 2')
+elif (x < 15):
+    print('Sono nel caso 3')
+
+
+
+# funzioni ricorsive: cioè una funzione può richiamare se stessa
+
+# esempio
+
+def contoArovescia(n):
+    if n <= 0:
+        print('Via!')
+    else:
+        print(n)
+        contoArovescia(n - 1)
+
+
+print(contoArovescia(5)) # stampa 5, 4, 3, 2, 1, Via!
