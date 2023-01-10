52e2fd4aea6edb5e849f08b05af35e98f0e869ea
Lorenzo Salvi <lorenzosalvi@MacBook-Air-di-Lorenzo.local>
2023-01-10 11:15:41 +0100

    aggiornamento logica applicazione in flask

--- esempi-test-tesi/file.py
+++ esempi-test-tesi/file.py
@@ -10,34 +10,30 @@ x = \
     "id": "Richiesta 01",
     "value": "Schedulare la/e risorse sul nodo Slave One!",
     "datetime": str(dateTime)
-},{
+}
+
+x.update({
     "id": "Richiesta 02",
     "value": "Schedulare la/e risorse sul nodo Slave Two!",
-    "date": str(dateTime),
-}
+    "datetime": str(dateTime)
+})
 
 y = json.dumps(x)
 
-print(y)
+#print(y)
 
+with open('jsonfile.json', 'w') as myFile:
+    myFile.write(y)
 
 
 # da formato json a oggetto "array" python
-
-var_json = '{ "id":"Richiesta 01", "value":"Schedulare la/e risorse sul nodo Slave One!"}'
-
+#var_json = '{ "id":"Richiesta 01", "value":"Schedulare la/e risorse sul nodo Slave One!"}'
 #var_json.append('{ "id":"Richiesta 02", "value":"Schedulare la/e risorse sul nodo Slave Two!"}')
-
-array_python = json.loads(var_json)
-
+#array_python = json.loads(var_json)
 #print(array_python)
 
 #
-
-array = ['{"id":"Richiesta 01","value":"Schedulare la/e risorse sul nodo Slave One!"}']
-
-jsonVar = json.dumps(array)
-
-print(array[0])
-
-print(jsonVar)
+#array = ['{"id":"Richiesta 01","value":"Schedulare la/e risorse sul nodo Slave One!"}']
+#jsonVar = json.dumps(array)
+#print(array[0])
+#print(jsonVar)
