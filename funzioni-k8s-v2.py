8c802ce3e60ead8b0a4a73dfa866edfe686e965c
Lorenzo Salvi <lorenzosalvi@iMac-di-Lorenzo.homenet.telecomitalia.it>
2023-03-13 19:31:06 +0100

    creazione logica v2 del 13/03/2023

--- funzioni-k8s-v2.py
+++ funzioni-k8s-v2.py
@@ -1,6 +1,7 @@
 import json
 import re
 import random
+import time
 
 #def _get_schedulable_node(v1_client):
 #    node_list = _get_ready_nodes(v1_client)
@@ -17,22 +18,22 @@ def _get_ready_nodes():
 
     return ready_nodes
 
-node_list = _get_ready_nodes()     #node_list = ['docker-desktop', 'slave-one', ''slave-two]
+#node_list = _get_ready_nodes()     #node_list = ['docker-desktop', 'slave-one', ''slave-two]
 #print(node_list[0])        stampa: docker desktop
 #print(node_list[1])        stampa: slave-one
 #print(node_list[2])        stampa: slave-two
 
-avalaible_node = list(set(node_list))  #available_node = [''slave-two', 'slave-one', 'docker-desktop']
+#avalaible_node = list(set(node_list))  #available_node = [''slave-two', 'slave-one', 'docker-desktop']
 #print(avalaible_node)
 
 #random_node = random.choice(avalaible_node)
 #print(random_node)
 
 # apro il file json
-read_file = open('file.json')
+#read_file = open('folder/file.json')
 
 # restituisco l'oggetto json come dizionario
-data = json.load(read_file)
+#data = json.load(read_file)
 
 # stampo il valore riferito al campo specificato
 #print(data['datetime'])
@@ -40,19 +41,19 @@ data = json.load(read_file)
 #print(data['node-name'])
 
 # chiudo il file
-read_file.close()
+#read_file.close()
 
 # estraggo i valori dei nodi in available_node e faccio un matching
 # con il valore del campo node-name estratto dal dizionario data in
 # riferimento ai campi del file jsos
-specific_node = [item for item in avalaible_node
-                     if re.search(data['node-name'], item) is not None]
+#specific_node = [item for item in avalaible_node
+#                     if re.search(data['node-name'], item) is not None]
 
-print(specific_node)        # stampa: ['docker-desktop']
+#print(specific_node)        # stampa: ['docker-desktop']
 
 # trasformo oggetto in stringa
-node = " ".join(specific_node)
-print(node)     # stampa: docker-desktop
+#node = " ".join(specific_node)
+#print(node)     # stampa: docker-desktop
 
 def _get_schedulable_node():
 
@@ -60,6 +61,54 @@ def _get_schedulable_node():
     if not node_list:
         return None
 
-    avalaible_node = list(set(node_list)) # array ordinato
+    avalaible_node = list(set(node_list))   # array ordinato
 
-    return
+    read_file = open('folder/file.json')    # apro file.json
+    data = json.load(read_file)     # restituisco l'oggetto json come dizionario
+    read_file.close()   # chiudo file.json
+
+    specific_node = [item for item in avalaible_node
+                         if re.search(data['node-name'], item) is not None]
+
+    node = " ".join(specific_node)  # trasformo oggetto in stringa
+
+    #time.sleep(60)  # timer di 60 sec
+
+    return node
+
+
+print(_get_ready_nodes())   # stampa: ['docker-desktop', 'slave-one', 'slave-two']
+print(_get_schedulable_node())  # stampa: docker-desktop
+
+#####################
+
+# per leggere il contenuto di un file e
+# sovrascriverlo successivamente
+
+# legge il contenuto del file
+read_data = open('folder/podname.txt', 'r')
+print(read_data.read())
+read_data.close()
+
+# sovrascrive il nuovo contenuto nello stesso file
+new_pod_name = "annotation-second-scheduler-four"
+write_data = open('folder/podname.txt', 'w')
+write_data.write(new_pod_name)
+write_data.close()
+
+read_data = open('folder/podname.txt', 'r')
+print(read_data.read())
+
+#####################
+
+# per aprire due file e copiare il contenuto di uno nell'altro
+
+# per aprire i due file
+with open('folder/file.json', 'r') as firstfile, open('templates/data.json', 'a') as secondfile:
+
+    # legge il contenuto dal primo file
+    for line in firstfile:
+        # copia il contenuto nel secondo file
+        secondfile.write(line)
+
+
